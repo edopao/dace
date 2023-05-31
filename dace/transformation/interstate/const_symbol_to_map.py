9c3bdf3f60d3e0aa792474bba4ad7177d20c7d1e
Edoardo Paone <edoardo.paone@cscs.ch>
2023-05-31 16:46:06 +0200

    New transformation 'ConstSymbolToMap'
    Add transformation to extract inter-state symbols used as constants
    in other states. This pattern is recurrent in sparse-matrix vector
    multiplication, in which an array stores the offset of row start.
    By moving the definition of these symbols into a map, it is possible
    to expand the nested sdfg and obtain a flat sdfg representation.

---
+++ dace/transformation/interstate/const_symbol_to_map.py
@@ -0,0 +1,139 @@
+import re
+
+from dace.sdfg import graph
+
+from dace.properties import make_properties, SubsetProperty
+from dace import Memlet, subsets, memlet
+from dace import nodes, sdfg as sd
+from dace.transformation import transformation as xf
+
+
+def reroute_edge_through_map(state, sdfg: sd.SDFG, e: graph.Edge, m: nodes.Map):
+    def is_map_symbol(s):
+        return s in m.map.params
+
+    use_memlet_path = True
+    if isinstance(m, nodes.MapEntry):
+        data = e.src.data
+        if any(map(is_map_symbol, e.data.src_subset.free_symbols)):
+            use_memlet_path = False
+    else:
+        data = e.dst.data
+        if any(map(is_map_symbol, e.data.dst_subset.free_symbols)):
+            use_memlet_path = False
+
+    if use_memlet_path:
+        state.add_memlet_path(e.src, m, e.dst, src_conn=e.src_conn, dst_conn=e.dst_conn, memlet=e.data)
+    else:
+        connector_type = sdfg.arrays[data].dtype
+        in_connector_name = "IN_" + data
+        m.add_in_connector(in_connector_name, connector_type)
+        out_connector_name = "OUT_" + data
+        m.add_out_connector(out_connector_name, connector_type)
+        data_subset_str = "0:{}".format(sdfg.arrays[data].total_size)
+
+        if isinstance(m, nodes.MapEntry):
+            state.add_edge(e.src, e.src_conn, m, in_connector_name, memlet.Memlet(data=data, subset=data_subset_str))
+            state.add_edge(m, out_connector_name, e.dst, e.dst_conn, e.data)
+        else:
+            state.add_edge(e.src, e.src_conn, m, in_connector_name, e.data)
+            state.add_edge(m, out_connector_name, e.dst, e.dst_conn, memlet.Memlet(data=data, subset=data_subset_str))
+
+    state.remove_edge(e)
+
+@make_properties
+class ConstSymbolToMap(xf.MultiStateTransformation):
+    symbol_state = xf.PatternNode(sd.SDFGState)
+    compute_state = xf.PatternNode(sd.SDFGState)
+    symbol_edge = None
+    memlet_symbols = {}
+
+    @classmethod
+    def expressions(cls):
+        maptoloop_sdfg = sd.graph.OrderedDiGraph()
+        maptoloop_sdfg.add_nodes_from([cls.symbol_state, cls.compute_state])
+        maptoloop_sdfg.add_edge(cls.symbol_state, cls.compute_state, sd.InterstateEdge())
+        return [maptoloop_sdfg]
+
+    # Some naming conventions:
+    # - Source node is called symbol state, we check if there is an out-edge which defines some symbols
+    #   as array access.
+    # - The next state is called compute state, and for now has to be a sink state: this is a pure restriction
+    #   to be on the safe side, and ensure that the interstate symbols are not used anywhere else;
+    #   the strict requirement would just be that the symbols are not modified, so they can be treated as constants.
+    def can_be_applied(self, graph: sd.SDFGState, expr_index: int, sdfg: sd.SDFG, permissive: bool = False):
+        # For now put restriction that compute state is a sink node
+        if len(graph.out_edges(self.compute_state)) != 0:
+            return False
+        # Find the edge that connects the symbol node to the compute node
+        for e in graph.out_edges(self.symbol_state):
+            if e.dst == self.compute_state:
+                self.symbol_edge = e
+                break
+        assert(self.symbol_edge != None)
+        # Find inter-state symbols on this edge used by the compute node
+        # N.B. should be all edges as long as the compute node is a sink node
+        for k, v in self.symbol_edge.data.assignments.items():
+            if k in self.compute_state.free_symbols:
+                # select only the assignments that contain array access
+                try:
+                    m = Memlet(v)
+                except SyntaxError:
+                    continue
+                if m.subset != None and m.data in sdfg.arrays:
+                    self.memlet_symbols[k] = m
+        return len(self.memlet_symbols) != 0
+
+    def apply(self, graph, sdfg: sd.SDFG):
+        map = nodes.Map(self.compute_state.label + "_map", [], [])
+        me = nodes.MapEntry(map)
+        mx = nodes.MapExit(map)
+
+        # replace inter-state symbols with map
+        id_table = {}
+        for s, m in self.memlet_symbols.items():
+            # check that symbol is not defined yet
+            assert (s not in map.params)
+            # create in-connector with unique id
+            if m.data not in id_table.keys():
+                id_table[m.data] = 1
+            else:
+                id_table[m.data] += 1
+            connector_name = m.data + '_' + str(id_table[m.data])
+            connector_type = sdfg.arrays[m.data].dtype
+            me.add_in_connector(connector_name, connector_type)
+            # create access node, if not already existing
+            access_node = None
+            for n in self.compute_state.source_nodes():
+                if isinstance(n, nodes.AccessNode) and n.data == m.data:
+                    access_node = n
+                    break
+            if not access_node:
+                access_node = nodes.AccessNode(m.data)
+                self.compute_state.add_node(access_node)
+            # create edge from access node to map entry node
+            self.compute_state.add_edge(access_node, None, me, connector_name, m)
+            # update table of symbols in map range
+            map.params.append(s)
+            map.range += subsets.Range(SubsetProperty.from_string(connector_name))            # TODO remove inter-state symbols
+            self.symbol_edge.data.assignments.pop(s)
+
+        # search for source nodes and corresponding edges
+        for n in self.compute_state.source_nodes():
+            # ignore the access nodes newly added for map symbols
+            if n.data not in id_table.keys():
+                for e in self.compute_state.edges():
+                    if e.src == n:
+                        reroute_edge_through_map(self.compute_state, sdfg, e, me)
+
+        # search for sink nodes and corresponding edges
+        for n in self.compute_state.sink_nodes():
+            # ignore the access nodes newly added for map symbols
+            if n.data not in id_table.keys():
+                for e in self.compute_state.edges():
+                    if e.dst == n:
+                        reroute_edge_through_map(self.compute_state, sdfg, e, mx)
+
+        # removal of symbol state
+        if len(self.symbol_edge.data.assignments) == 0 and len(self.symbol_state.nodes()) == 0:
+            graph.remove_node(self.symbol_state)
---
+++ tests/transformations/const_symbol_to_map_test.py
@@ -0,0 +1,131 @@
+import unittest
+
+import numpy as np
+
+import dace
+from dace.libraries.blas import MatMul
+from dace.sdfg import nodes
+from dace.transformation.interstate import LoopToMap
+from dace.transformation.interstate.const_symbol_to_map import ConstSymbolToMap
+
+
+def make_spmv_sdfg():
+    sdfg = dace.SDFG(f"spmv")
+    init = sdfg.add_state("init")
+    guard = sdfg.add_state("guard")
+    state = sdfg.add_state("state")
+    body = sdfg.add_state("body")
+    after = sdfg.add_state("after")
+
+    M, N, nnz = (dace.symbol(s, dtype=dace.uint32) for s in ('M', 'N', 'nnz'))
+
+    sdfg.add_array('A_row', [M + 1], dace.uint32)
+    sdfg.add_array('A_col', [nnz], dace.uint32)
+    sdfg.add_array('A_val', [nnz], dace.float64)
+    sdfg.add_array('x', [N], dace.float64)
+    sdfg.add_array('y', [M], dace.float64)
+
+    sdfg.add_edge(init, guard, dace.InterstateEdge(assignments={"i": "0"}))
+    sdfg.add_edge(guard, state, dace.InterstateEdge(condition="i < M"))
+    sdfg.add_edge(guard, after, dace.InterstateEdge(condition="i >= M"))
+    sdfg.add_edge(state, body, dace.InterstateEdge(
+        assignments={"__start": "A_row[i]", "__stop": "A_row[i + 1]"}))
+    sdfg.add_edge(body, guard, dace.InterstateEdge(assignments={"i": "i + 1"}))
+
+    # Create access nodes for reading and writing
+    A_col_node = body.add_access('A_col')
+    A_val_node = body.add_access('A_val')
+    x_node = body.add_access('x')
+    y_node = body.add_access('y')
+    # Create transient nodes for temporary storage
+    x_slice_node = body.add_transient('x_slice', [N], dace.float64)
+
+    # Create a map in which a tasklet will reside
+    me, mx = body.add_map('indirect_x_slice', dict(__i0='__start:__stop'))
+    # Create tasklet for indirection of x values
+    indirection_tasklet = body.add_tasklet('indirection', {'__arr', '__inp0'}, {'__out'}, '__out = __arr[__inp0]')
+    # input path (src->me->tasklet[a])
+    body.add_memlet_path(x_node, me, indirection_tasklet, dst_conn='__arr',
+                         memlet=dace.Memlet(data='x', subset='0:N'))
+    body.add_memlet_path(A_col_node, me, indirection_tasklet, dst_conn='__inp0',
+                         memlet=dace.Memlet(data='A_col', subset='__i0'))
+    # output path (tasklet[b]->mx->dst)
+    body.add_memlet_path(indirection_tasklet, mx, x_slice_node, src_conn='__out',
+                         memlet=dace.Memlet(data='x_slice', subset='__i0 - __start'))
+
+    matmult_tasklet = MatMul('_MatMult_')
+    body.add_node(matmult_tasklet)
+    body.add_memlet_path(A_val_node, matmult_tasklet, dst_conn='_a',
+                         memlet=dace.Memlet(data='A_val', subset='__start:__stop'))
+    body.add_edge(x_slice_node, None, matmult_tasklet, '_b',
+                  dace.Memlet(data='x_slice', subset='0:__stop - __start'))
+    body.add_edge(matmult_tasklet, '_c', y_node, None, dace.Memlet(data='y', subset='i'))
+
+    return sdfg
+
+def make_sparse_matrx(M, N, nnz):
+    from numpy.random import default_rng
+    rng = default_rng(42)
+
+    x = rng.random((N, ))
+
+    from scipy.sparse import random
+
+    matrix = random(M,
+                    N,
+                    density=nnz / (M * N),
+                    format='csr',
+                    dtype=np.float64,
+                    random_state=rng)
+    rows = np.uint32(matrix.indptr)
+    cols = np.uint32(matrix.indices)
+    vals = np.copy(matrix.data)
+
+    return rows, cols, vals, x
+
+def spmv_ref(A_row, A_col, A_val, x):
+    y = np.empty(A_row.size - 1, A_val.dtype)
+
+    for i in range(A_row.size - 1):
+        cols = A_col[A_row[i]:A_row[i + 1]]
+        vals = A_val[A_row[i]:A_row[i + 1]]
+        y[i] = vals @ x[cols]
+
+    return y
+
+class TestConstSymbolToMap(unittest.TestCase):
+
+    def test_const_symbol_to_map(self):
+        sdfg = make_spmv_sdfg()
+
+        num_transformations = sdfg.apply_transformations([LoopToMap])
+        self.assertEqual(1, num_transformations)
+        # run simplify to eliminate the exit state, so that the sink node can be detected
+        sdfg.simplify()
+
+        self.assertEqual(1, len(sdfg.nodes()))
+        self.assertEqual(['loop_body'], [n.label for n in sdfg.nodes()[0] if isinstance(n, nodes.NestedSDFG)])
+
+        num_transformations = sdfg.apply_transformations([ConstSymbolToMap])
+        self.assertEqual(1, num_transformations)
+        sdfg.simplify()
+
+        # test that after transformation there is no nested sdfg
+        self.assertEqual(1, len(sdfg.nodes()))
+        self.assertEqual([], [n.label for n in sdfg.nodes()[0] if isinstance(n, nodes.NestedSDFG)])
+
+        M = np.uint32(4096)
+        N = np.uint32(8192)
+        nnz = np.uint32(2048)
+
+        A_row, A_col, A_val, x = make_sparse_matrx(M, N, nnz)
+        y = np.empty(M, dtype=x.dtype)
+
+        sdfg(A_row=A_row, A_col=A_col, A_val=A_val, x=x, y=y, M=M, N=N, nnz=nnz)
+
+        if not all(np.isclose(y, spmv_ref(A_row, A_col, A_val, x))):
+            raise ValueError("Validation failed.")
+
+
+if __name__ == '__main__':
+    unittest.main()
