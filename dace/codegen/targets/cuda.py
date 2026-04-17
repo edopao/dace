4a322c1cd4c13cf6884dde958a0efaee9c358d8b
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2026-04-17 19:51:04 +0200

    Handle HIP flags similarly to CUDA flags (#2338)
    - Fix script to query the HIP architecture of the machine
    - Remove explicit setting of `--offload-arch` in `HIP_HIPCC_FLAGS`
    - Set `CMAKE_HIP_FLAGS` instead of `EXTRA_HIP_FLAGS`

--- dace/codegen/targets/cuda.py
+++ dace/codegen/targets/cuda.py
@@ -549,10 +549,8 @@ void __dace_gpu_set_all_streams({sdfg_state_name} *__state, gpuStream_t stream)
             hip_arch = [ha for ha in hip_arch if ha is not None and len(ha) > 0]
 
             flags = Config.get("compiler", "cuda", "hip_args")
-            flags += ' ' + ' '.join(
-                '--offload-arch={arch}'.format(arch=arch if arch.startswith("gfx") else "gfx" + arch)
-                for arch in hip_arch)
-            options.append("-DEXTRA_HIP_FLAGS=\"{}\"".format(flags))
+            options.append(f'-DDACE_HIP_ARCHITECTURES_DEFAULT="{";".join(hip_arch)}"')
+            options.append("-DCMAKE_HIP_FLAGS=\"{}\"".format(flags))
 
         if Config.get('compiler', 'cpu', 'executable'):
             host_compiler = make_absolute(Config.get("compiler", "cpu", "executable"))
